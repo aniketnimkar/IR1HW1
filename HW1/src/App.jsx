ce1fa85eb2bd1d6ddb4f4519f6b48d237ea6df86
aniketnimkar <aniketnimkar88@gmail.com>
2024-03-21 14:20:06 +0000

    Moved to HW1

---
+++ HW1/src/App.jsx
@@ -0,0 +1,71 @@
+import "./App.css";
+const Heading = () => {
+  return <h1>React Playground</h1>;
+};
+
+const Greetings = () => {
+  return <h1>Hello, Alice Smith!</h1>;
+};
+
+const ExtrenalLink = () => {
+  const url = "https://www.reactexamples.com";
+  return (
+    <a href={url} target="_blank">
+      Visit React Examples
+    </a>
+  );
+};
+
+const Avtar = () => {
+  const imgUrl = "https://via.placeholder.com/200";
+  return (
+    <div>
+      <img src={imgUrl} />
+    </div>
+  );
+};
+
+const UserInfo = () => {
+  const user = {
+    name: "Alice Smith",
+    age: 28,
+    email: "alice.smith@example.com",
+    bio: "Passionate about React and building innovative web applications.",
+  };
+
+  return (
+    <div>
+      <h1>User Information</h1>
+      <p>
+        <strong>Name:</strong>
+        {user.name}
+      </p>
+      <p>
+        <strong>Age:</strong>
+        {user.age}
+      </p>
+      <p>
+        <strong>Ege:</strong>
+        {user.email}
+      </p>
+      <p>
+        <strong>Bio:</strong>
+        {user.bio}
+      </p>
+    </div>
+  );
+};
+
+export default function App() {
+  return (
+    <div>
+      <Heading />
+      <Greetings />
+      <ExtrenalLink />
+      <Avtar />
+      <UserInfo />
+    </div>
+  );
+}
+
+// React
